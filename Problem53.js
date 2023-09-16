21458388717e8065ae7fd0e893e60b6aad784043
Nihar1420 <niharranjanhota863@gmail.com>
2023-09-16 23:56:23 +0530

    Problem 53 solved

---
+++ Problem53.js
@@ -0,0 +1,28 @@
+// We will use reccursion to solve the factorial of a number problem .
+
+// Normal method -
+
+// let factNum = 1;
+// for(let i = 2; i <= 4 ; i++){
+//    factNum *= i;
+// }
+
+// console.log(factNum);
+
+// let factNum = 1;
+// for(let i = 4; i >= 1 ; i--){
+//    factNum *= i;
+// }
+
+// console.log(factNum);
+
+// Reccursion -
+
+function reccursion(num) {
+  if (num === 0) {
+    return 1;
+  }
+    return num * reccursion(num - 1);
+}
+
+console.log(reccursion(5));
