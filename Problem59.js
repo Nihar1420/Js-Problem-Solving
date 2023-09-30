bbaf3f385c16a5520a59c5fad77b7a2341e7c2c7
Nihar1420 <niharranjanhota863@gmail.com>
2023-09-30 23:50:29 +0530

    Problem 59 solved

---
+++ Problem59.js
@@ -0,0 +1,19 @@
+// Today we will practise reccursion using a small example of factorial -
+
+// As we know the steps are finding the base point and reccursive relation -
+
+// const factOfNo = (num) => {
+//   if (num === 0) {
+//     return 1;
+//   } else {
+//     return num * factOfNo(num - 1);
+//   }
+// };
+
+// Product of 2 numbers using reccursion -
+
+// const ProductOfNumbers = (num1) => {
+//    if(num)
+// }
+
+console.log(factOfNo(4));
